975ba3f6d6a166d0c99a7012b5df7b0bc506b3a7
Jimin L <69053105+chocothelee@users.noreply.github.com>
2021-11-24 20:42:05 +0700

    Add files via upload
    - functions (load and draw)
    - set screen size
    - display text on screen

---
+++ main.lua
@@ -0,0 +1,13 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true})
+end
+
+function love.draw()
+    love.graphics.printf("Welcome to PONG", 0, WINDOW_HEIGHT/2, WINDOW_WIDTH, 'center')
+end
